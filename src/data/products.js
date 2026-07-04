4e66260ac048201df87c34472a3c857b9db67380
OnelMartinez <onelmartinezv@gmail.com>
2026-07-04 16:14:46 -0400

    fix(design): añadir pagina de menu y login

---
+++ src/data/products.js
@@ -0,0 +1,69 @@
+// Datos mock - solo pizzas por ahora (tab activa en el diseño)
+export const products = [
+  {
+    id: 1,
+    name: "Pizza de Queso Gouda",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+  {
+    id: 2,
+    name: "Pizza de Salchicha",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+  {
+    id: 3,
+    name: "Pizza Hawaiana",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+  {
+    id: 4,
+    name: "Pizza de Atún",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+  {
+    id: 5,
+    name: "Pizza de Salchicha",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+  {
+    id: 6,
+    name: "Pizza Bacon",
+    description:
+      "Base crocante, salsa de tomate natural y una generosa capa de queso Gouda derretido hasta el punto justo",
+    price: 750,
+    image: "/placeholder_img.jpg",
+  },
+];
+
+export const categories = [
+  "Cafés",
+  "Batidos",
+  "Smoothies",
+  "Antojitos",
+  "Cocteles",
+  "Lasanas",
+  "Snaks",
+  "Pizzas",
+  "Pastas",
+  "Desayunos",
+  "Carnes",
+  "Taquitos",
+  "Hamburguesas",
+  "Sandwiches",
+  "Agregos",
+];
